7c6ed518ca6482bcf45e58fb4379280a9507d502
Kayode Oluwasegun <kay_1oneway@yahoo.com>
2025-05-27 13:43:12 +0100

    comments added

--- day4_functions/even_or_odd.py
+++ day4_functions/even_or_odd.py
@@ -1,7 +1,11 @@
 # 3. Function that checks if a number is even or odd
 
+# Get number from user
 number = int(input("Enter number: "))
 
+# Function definition
 def even_or_odd(num):
     print("It's Even") if num % 2 == 0 else print("It's Odd")
+
+# Function call
 even_or_odd(number)
