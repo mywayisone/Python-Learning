07eee3428664b2963ba8d9da98091d85488d0e5e
Kayode Oluwasegun <kay_1oneway@yahoo.com>
2025-05-28 21:23:35 +0100

    Add Day 6 Exercises

---
+++ day6_dicts/update_student.py
@@ -0,0 +1,13 @@
+# 2. Add New Key to Dictionary
+# Add "phone" to the student dictionary
+
+student = {
+
+    "name" : "Kayode Oluwasegun",
+    "email" : "kayodesegun2017@gmail.com",
+    "age" : 25,
+    "grade" : "A",
+}
+
+student["phone"] = "08065942879"
+print(student.values())
