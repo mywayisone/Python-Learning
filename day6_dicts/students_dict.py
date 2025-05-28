2c725e752fc3c8f2924fcbf7e72ddd2e2f6f987e
Kayode Oluwasegun <kay_1oneway@yahoo.com>
2025-05-28 21:22:58 +0100

    Add Day 6 Exercises

---
+++ day6_dicts/students_dict.py
@@ -0,0 +1,13 @@
+# 1. Create a Dictionary of a Student
+# Fields: name, age, grade, email
+# Print all values
+
+student = {
+
+    "name" : "Kayode Oluwasegun",
+    "email" : "kayodesegun2017@gmail.com",
+    "age" : 25,
+    "grade" : "A",
+}
+
+print(student.values())
