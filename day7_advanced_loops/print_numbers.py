915088799d63739d52dc1a96728d006ea4f0c973
Kayode Oluwasegun <kay_1oneway@yahoo.com>
2025-05-30 19:33:58 +0100

    Add Day 7 Exercises

---
+++ day7_advanced_loops/print_numbers.py
@@ -0,0 +1,13 @@
+# 1. print_numbers.py
+# - Print numbers from 1 to 10 using a for loop
+
+def print_numbers():
+    for i in range(1, 11):
+        print(i)
+
+def main():
+    print("\n Printing 1 - 10 below: \n")
+    print_numbers()
+
+if __name__ == "__main__":
+    main()
