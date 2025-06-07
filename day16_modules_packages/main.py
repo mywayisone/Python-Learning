19b3f0b802062989eb398e26837c9422672434a5
Kayode Oluwasegun <kay_1oneway@yahoo.com>
2025-06-07 07:22:17 +0100

    Add Day 16 Exercises

---
+++ day16_modules_packages/main.py
@@ -0,0 +1,7 @@
+from calculator.basic_ops import add, subtract
+from calculator.advanced_ops import multiply, divide
+
+print("Add:", add(2, 3))
+print("Subtract:", subtract(5, 2))
+print("Multiply:", multiply(3, 4))
+print("Divide:", divide(10, 2))
