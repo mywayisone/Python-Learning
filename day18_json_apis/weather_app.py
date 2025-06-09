7e72fe5c25bd431f11ff0ecedccdd70978ef146c
Kayode Oluwasegun <kay_1oneway@yahoo.com>
2025-06-09 23:29:20 +0100

    Add Day 18 Exercises

--- day18_json_apis/weather_app.py
+++ day18_json_apis/weather_app.py
@@ -13,8 +13,16 @@
 # 3. Add error handling (e.g., if the server is down)
 import requests
 
-response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=6.5244&longitude=3.3792&current_weather=true")
-if response.status_code == 200:
+try:
+    response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=6.5244&longitude=3.3792&current_weather=true")
+    response.raise_for_status()
+    
+except requests.exceptions.HTTPError as http_err:
+    print(f"Http error occured: {http_err}")
+except requests.exceptions.ConnectionError as conn_err:
+    print(f"Connection error: {conn_err}")
+except requests.exceptions.RequestException as err:
+    print(f"Error: {err}")
+else:
     data = response.json()
     print(f"Temperature : {data["current_weather"]["temperature"]}\nWind speed: {data["current_weather"]["windspeed"]}\nTime of the weather update: {data["current_weather"]["time"]}")
-else: print(f"Something went wrong: {response.status_code} error")
